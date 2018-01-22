270ce0a455febcda5687de6afb9630ce3aebddd2
Ivan Brygar <ivb@sitecore.net>
2018-01-22 15:11:20 +0800

    Added navigation part between charts.

--- SCPersonalizationGraph/UI/src/app/app.component.ts
+++ SCPersonalizationGraph/UI/src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  personalizationChart = false;
+  barChart = false;
+  piChart = false;
+
+  doPersonalization(){
+    this.personalizationChart = true;
+    this.barChart = false;
+    this.piChart = false;
+  }
+
+  doBar(){
+    this.personalizationChart = false;
+    this.barChart = true;
+    this.piChart = false;
+  }
+
+  doPi(){
+    this.personalizationChart = false;
+    this.barChart = false;
+    this.piChart = true;
+  }
 }
